ae2207ddf41a2def356c5798f68de9a8e8955dea
Alexandre Silva <contato@silvalexandre.com>
2017-09-21 14:50:14 +0000

    fixes path & adds it to ignore

--- server/index.js
+++ server/index.js
@@ -2,18 +2,18 @@
 
 const ytDown = require('..');
 
-ytDown.saveAudio('https://www.maneiro.com/watch?v=iy4mXZN1Zzk', 'output2.mp3').then(() => {
+ytDown.saveAudio('https://www.maneiro.com/watch?v=iy4mXZN1Zzk', './output/audio.mp3').then(() => {
 	console.log('done audio');
 }).catch(e => {
 	console.log('error');
 });
-ytDown.saveMp4('https://www.youtube.com/watch?v=iy4mXZN1Zzk', 'video.mp4').then(() => {
+ytDown.saveMp4('https://www.youtube.com/watch?v=iy4mXZN1Zzk', './output/video.mp4').then(() => {
 	console.log('done video');
 }).catch(e => {
 	console.log('error');
 });
 
-ytDown.saveVideo('https://www.youtube.com/watch?v=iy4mXZN1Zzk', 'video_na.mp4').then(() => {
+ytDown.saveVideo('https://www.youtube.com/watch?v=iy4mXZN1Zzk', './output/video_no_audio.mp4').then(() => {
 	console.log('done video with no audio');
 }).catch(e => {
 	console.log('error');
