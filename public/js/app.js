3092b3b28a5f44cae6d6da90b640635788d3aee0
Alexandre Silva <contato@silvalexandre.com>
2017-09-22 00:26:29 +0000

    improves UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,29 @@
 /*global axios*/
 (function() {
-    "use strict";
-    var form = document.getElementById('form');
-    form.addEventListener('submit', function(evt) {
-        evt.preventDefault();
-        getInfo();
-    });
+	"use strict";
+	var form = document.getElementById('form');
+	form.addEventListener('submit', evt => {
+		evt.preventDefault();
+		getInfo();
+	});
 
-    function getInfo() {
-        var linkEl = document.getElementById('link');
-        axios.get('/api/?link=' + linkEl.value).then(function(response) {
-            setInfo(response.data);
-        }).catch(function(error) {
-            console.log(error);
-            alert('Server unavailable');
-        });
-    }
+	function getInfo() {
+		const linkEl = document.getElementById('link');
+		axios.get('/api/?link=' + linkEl.value).then(response => {
+			setInfo(response.data);
+		}).catch(error => {
+			console.log(error);
+			alert('Server unavailable');
+		});
+	}
 
-    function setInfo(video) {
-        document.getElementById('title').innerText = video.data.title;
-        document.getElementById('uploaded').innerHTML = '<strong>Uploaded by:</strong> ' + video.data.uploaded;
-        document.getElementById('length').innerHTML = '<strong>Length:</strong> ' + video.data.length;
-        document.getElementById('asmp3').href = "download/mp3/" + video.v;
-        document.getElementById('asmp4').href = "download/mp4/" + video.v;
-    }
+	function setInfo(video) {
+		document.getElementById('title').innerText = video.data.title;
+		document.getElementById('uploaded').innerHTML = `<strong>Uploaded by:</strong> ${video.data.uploaded}`;
+		document.getElementById('length').innerHTML = `<strong>Length:</strong>  ${video.data.length}`;
+		document.getElementById('asmp3').href = `download/mp3/${video.v}`;
+		document.getElementById('asmp4').href = `download/mp4/${video.v}`;
+		document.getElementById('asmp4muted').href = `download/mp4/${video.v}/muted`;
+		document.getElementById('card').style.display = "block";
+	}
 })();
